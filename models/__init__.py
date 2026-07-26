e6361dda292d290334f12a3b47202907fa487413
kkianamm <kiana.taktehrani@gmail.com>
2026-07-26 00:34:32 -0400

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -5,7 +5,8 @@ from .dlinear import DLinear
 from .FEDformer import FEDformer
 from .PatchTST import PatchTST
 from .TimesNet import TimesNet
-
+from .mira_biomedbert import MiraBiomedBERT
+model_lookup["mira_biomedbert"] = MiraBiomedBERT
 
 model_lookup = {
 	"timellm": MedTsLLM,
